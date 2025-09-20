b17e77215828388fa2ef873ca95ec2523edf7979
miladimnich <mila.land2204@gmail.com>
2025-09-20 22:59:51 +0200

    added feedback service

--- src/main/java/com/example/telegram_bot/service/FeedbackService.java
+++ src/main/java/com/example/telegram_bot/service/FeedbackService.java
@@ -1,4 +1,18 @@
 package com.example.telegram_bot.service;
 
+import com.example.telegram_bot.entity.Feedback;
+import com.example.telegram_bot.repository.FeedbackRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedbackService {
+    private final FeedbackRepository repository;
+
+    public FeedbackService(FeedbackRepository repository) {
+        this.repository = repository;
+    }
+
+    public Feedback saveFeedback(Feedback feedback) {
+        return repository.save(feedback);
+    }
 }
