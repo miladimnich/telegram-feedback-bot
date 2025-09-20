c27184c56cbaebf97e9c5ef198062b02e385a080
miladimnich <mila.land2204@gmail.com>
2025-09-20 22:58:32 +0200

    added Feedback class

--- src/main/java/com/example/telegram_bot/entity/Feedback.java
+++ src/main/java/com/example/telegram_bot/entity/Feedback.java
@@ -1,4 +1,24 @@
 package com.example.telegram_bot.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "feedbacks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String department;
+    private String position;
+    private String message;
+    private String emotion;
+    private int criticality;
+    private String solution;
 }
